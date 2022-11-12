3aff8da47f30b4a92cde4f61a82b55a2ee217aef
hqkhai <93475167+hqkhai@users.noreply.github.com>
2022-11-12 13:21:21 +0700

    static and http logger

--- src/index.js
+++ src/index.js
@@ -8,14 +8,27 @@ const route = require("./routes")
 const cookieParser = require("cookie-parser")
 const cors = require('cors')
 
+
+
+//static
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+
+
+//HTTP logger
+const morgan = require('morgan')
+app.use(morgan('combined'))
+
+
+//cors
 const whitelist = ['http://localhost:3000', 'http://127.0.0.1:8800', 'http://localhost:8800']
 const corsOptions = {
     origin: whitelist,
     optionsSuccessStatus: 200
 }
-
 app.use(cors(corsOptions))
-
 
 db.connect()
 
