2c23c1005f0244faf5416e1e63ff5515d6f75bd9
hqkhai <93475167+hqkhai@users.noreply.github.com>
2022-11-12 13:18:13 +0700

    cors fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const app = express()
 const db = require("./config/db")
 const route = require("./routes")
 const cookieParser = require("cookie-parser")
+const cors = require('cors')
+
+const whitelist = ['http://localhost:3000', 'http://127.0.0.1:8800', 'http://localhost:8800']
+const corsOptions = {
+    origin: whitelist,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
 
 db.connect()
 
