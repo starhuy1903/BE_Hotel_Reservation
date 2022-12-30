278937c27d0fa872a6e48270ad90ea37b5bc417a
hqkhai <93475167+hqkhai@users.noreply.github.com>
2022-12-30 16:43:41 +0700

    fix get user  history

--- src/app/service/History.js
+++ src/app/service/History.js
@@ -1,7 +1,28 @@
 const Reservation = require('../models/reservation')
-
-const getHistory = async (userId)=>{
-    return Reservation.find({userId: userId})
+const mongoose = require('mongoose')
+const getHistory = async (userId1)=>{
+    return Reservation.aggregate([
+        {   $lookup: {
+            from: "reservationstatusevents",
+            let: { reservationId: "$_id"},
+            pipeline: [
+                {$match: {$expr: {$and: [{ $eq: [ "$$reservationId", "$reservationId" ]},]}}},
+                { $lookup: {
+                    from: "reservation status catelogs",
+                    let: { reservationCatelogId: "$reservationStatusCatalogId"},
+                    pipeline: [
+                        {$match: {$expr: {$and: [{ $eq: [ "$$reservationCatelogId", "$_id" ]}]}}}
+                    ],
+                    as: "reservationStatusCatelog"   
+                }},
+                {$unwind: "$reservationStatusCatelog"},
+            ],
+            as: "reservationStatus" 
+        }
+    },
+    {$match: {userId: mongoose.Types.ObjectId(userId1)}},
+    //{$project: {statusName: "$reservationStatus.reservationStatusCatelog.statusName", userId: 1, startDate: 1, endDate: 1, discountPercent: 1, totalPrice: 1}}
+    ])
 }
 
 module.exports = {getHistory}
