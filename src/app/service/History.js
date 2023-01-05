6797e577b1886d1ea9e8649d49c6c7cca8245f36
hqkhai <93475167+hqkhai@users.noreply.github.com>
2023-01-05 18:02:06 +0700

    add some field to history

--- src/app/service/History.js
+++ src/app/service/History.js
@@ -4,7 +4,7 @@ const getHistory = async (userId) => {
   return Reservation.aggregate([
     {
       $lookup: {
-        from: "reservationstatusevents",
+        from: "reservation_status_events",
         let: { reservationId: "$_id" },
         pipeline: [
           {
@@ -14,27 +14,77 @@ const getHistory = async (userId) => {
           },
           {
             $lookup: {
-              from: "reservation status catelogs",
-              let: { reservationCatelogId: "$reservationStatusCatalogId" },
+              from: "reservation_status_catalogs",
+              let: { reservationCatalogId: "$reservationStatusCatalogId" },
               pipeline: [
                 {
                   $match: {
                     $expr: {
-                      $and: [{ $eq: ["$$reservationCatelogId", "$_id"] }],
+                      $and: [{ $eq: ["$$reservationCatalogId", "$_id"] }],
                     },
                   },
                 },
               ],
-              as: "reservationStatusCatelog",
+              as: "reservationStatusCatalog",
             },
           },
-          { $unwind: "$reservationStatusCatelog" },
+          { $unwind: "$reservationStatusCatalog" },
         ],
         as: "reservationStatus",
       },
     },
+    { $unwind: "$reservationStatus" },
+    {
+      $lookup: {
+        from: "served_rooms",
+        let: { reservationId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: { $and: [{ $eq: ["$$reservationId", "$reservationId"] }] },
+            },
+          },
+          {
+            $lookup: {
+              from: "rooms",
+              let: { roomId: "$roomId" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $and: [{ $eq: ["$$roomId", "$_id"] }],
+                    },
+                  },
+                },
+                {
+                  $lookup: {
+                    from: "hotels",
+                    let: { hotelId: "$hotel_id" },
+                    pipeline: [
+                      {
+                        $match: {
+                          $expr: {
+                            $and: [{ $eq: ["$$hotelId", "$_id"] }],
+                          },
+                        },
+                      },
+                    ],
+                    as: "hotel",
+                  },
+                },
+                { $unwind: "$hotel" },
+              ],
+              as: "room",
+            },
+          },
+          { $unwind: "$room" },
+        ],
+        as: "servedRoom",
+      },
+    },
+    { $unwind: "$servedRoom" },
     { $match: { userId: mongoose.Types.ObjectId(userId) } },
-    //{$project: {statusName: "$reservationStatus.reservationStatusCatelog.statusName", userId: 1, startDate: 1, endDate: 1, discountPercent: 1, totalPrice: 1}}
+    {$project: {statusName: "$reservationStatus.reservationStatusCatalog.statusName", startDate: 1, endDate: 1, discountPercent: 1, totalPrice: 1, roomName: "$servedRoom.room.room_name", hotelName: "$servedRoom.room.hotel.name"}}
   ]);
 };
 
