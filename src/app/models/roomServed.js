0d4b3d13742e14113633a4a0be2790ad1a617656
hqkhai <93475167+hqkhai@users.noreply.github.com>
2023-03-22 23:13:19 +0700

    docker

---
+++ src/app/models/roomServed.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomServedSchema = new Schema(
+  {
+    roomId: { type: Schema.ObjectId, required: true },
+    reservationId: { type: Schema.ObjectId, required: true },
+    price: { type: Number },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Room Served", roomServedSchema);
